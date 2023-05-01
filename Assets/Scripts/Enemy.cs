6f2b41d87d84f73017938b3deaccf7ceaff37a98
guskirkpatrick <89876834+guskirkpatrick@users.noreply.github.com>
2023-04-30 20:22:38 -0400

    EnemyPrefab

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float moveSpeed = 5;
-    private bool movingRight = true;
+    private bool movingRight=false; 
 
     // Update is called once per frame
+    void Start()
+    {
+        int random = Random.Range(0, 1);
+        if (random == 0)
+            movingRight = true;
+
+    }
     void Update()
     {
        if (movingRight)
@@ -19,11 +26,11 @@ public class Enemy : MonoBehaviour
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
         
-        if (transform.position.x >= 10f)
+        if (transform.position.x >= 12f)
         {
             movingRight = false;
         }
-        else if (transform.position.x <= -10f)
+        else if (transform.position.x <= -14f)
         {
             movingRight = true;
         }
